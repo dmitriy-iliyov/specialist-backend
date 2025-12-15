baced04dce7ec9b05996885d5eeb9961ca0bc403
dmitriy-iliyov <dmitriy.iliyov@gmail.com>
2025-12-16 00:30:19 +0100

    fix(account create event)

--- backend/modules/auth-module/src/main/java/com/specialist/auth/infrastructure/listeners/AccountCreateListener.java
+++ backend/modules/auth-module/src/main/java/com/specialist/auth/infrastructure/listeners/AccountCreateListener.java
@@ -1,6 +1,6 @@
 package com.specialist.auth.infrastructure.listeners;
 
-import com.specialist.auth.domain.account.models.events.AccountCreateEvent;
+import com.specialist.contracts.auth.AccountCreateEvent;
 import com.specialist.auth.infrastructure.message.services.ConfirmationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
