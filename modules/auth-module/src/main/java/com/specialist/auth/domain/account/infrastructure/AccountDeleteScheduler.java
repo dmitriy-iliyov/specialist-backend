2bb7ef2b6ee8c78e54512f53ff84c77a3d71d62c
dmitriy-iliyov <dmitriy.iliyov@gmail.com>
2026-02-22 16:39:06 +0100

    fix(constructor)

--- modules/auth-module/src/main/java/com/specialist/auth/domain/account/infrastructure/AccountDeleteScheduler.java
+++ modules/auth-module/src/main/java/com/specialist/auth/domain/account/infrastructure/AccountDeleteScheduler.java
@@ -19,25 +19,30 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
-@RequiredArgsConstructor
 @Slf4j
 public class AccountDeleteScheduler {
-
-    @Value("${api.account-delete.propagation-batch-size}")
-    public int DELETE_PROPAGATION_BATCH_SIZE;
-
-    @Value("${api.account-delete.stuck-batch-size}")
-    public int STUCK_BATCH_SIZE;
-
-    @Value("${api.account-delete.ttl-before-hard-delete}")
-    public Duration TTL_BEFORE_HARD_DELETE;
-
-    @Value("${api.account-delete.hard-delete-batch-size}")
-    public int HARD_DELETE_BATCH_SIZE;
 
     private final AccountDeleteTaskService service;
     private final AccountDeleteEventSender sender;
     private final AccountService accountService;
+    private final int deletePropagationBatchSize;
+    private final int stuckBatchSize;
+    private final Duration ttlBeforeHardDelete;
+    private final int hardDeleteBatchSize;
+
+    public AccountDeleteScheduler(AccountDeleteTaskService service, AccountDeleteEventSender sender, AccountService accountService,
+                                  @Value("${api.account-delete.propagation-batch-size}") int deletePropagationBatchSize,
+                                  @Value("${api.account-delete.stuck-batch-size}") int stuckBatchSize,
+                                  @Value("${api.account-delete.ttl-before-hard-delete}") Duration ttlBeforeHardDelete,
+                                  @Value("${api.account-delete.hard-delete-batch-size}") int hardDeleteBatchSize) {
+        this.service = service;
+        this.sender = sender;
+        this.accountService = accountService;
+        this.hardDeleteBatchSize = hardDeleteBatchSize;
+        this.ttlBeforeHardDelete = ttlBeforeHardDelete;
+        this.stuckBatchSize = stuckBatchSize;
+        this.deletePropagationBatchSize = deletePropagationBatchSize;
+    }
 
     @Scheduled(
             initialDelayString = "${api.account-delete.propagation.initial_delay}",
@@ -45,7 +50,7 @@ public class AccountDeleteScheduler {
     )
     public void publishSoftDeleteEvents() {
         List<ImmediatelyAccountDeleteEvent> events = service.findBatchByStatus(
-                AccountDeleteTaskStatus.READY_TO_SEND, AccountDeleteTaskStatus.SENDING, DELETE_PROPAGATION_BATCH_SIZE
+                AccountDeleteTaskStatus.READY_TO_SEND, AccountDeleteTaskStatus.SENDING, deletePropagationBatchSize
         );
         if (events.isEmpty()) {
             return;
@@ -67,7 +72,7 @@ public class AccountDeleteScheduler {
     )
     public void recoverStuckEvents() {
         service.findBatchByStatus(
-                AccountDeleteTaskStatus.SENDING, AccountDeleteTaskStatus.READY_TO_SEND, STUCK_BATCH_SIZE
+                AccountDeleteTaskStatus.SENDING, AccountDeleteTaskStatus.READY_TO_SEND, stuckBatchSize
         );
     }
 
@@ -76,11 +81,11 @@ public class AccountDeleteScheduler {
             fixedDelayString = "${api.account-delete.hard-delete.fixed_delay}"
     )
     public void publishHardDeleteEvents() {
-        Instant threshold = Instant.now().minusSeconds(TTL_BEFORE_HARD_DELETE.toSeconds());
+        Instant threshold = Instant.now().minusSeconds(ttlBeforeHardDelete.toSeconds());
         List<DeferAccountDeleteEvent> accounts = accountService.findAllByDisableReasonAndThreshold(
                 DisableReason.SOFT_DELETE,
                 threshold,
-                HARD_DELETE_BATCH_SIZE
+                hardDeleteBatchSize
         );
         accountService.deleteAllByIdIn(
                 accounts.stream()
